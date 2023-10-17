53b832b2aa06f5598b77e9e85c2342cfe4e58e1c
Onu Joshua <onujoshua2001@gmail.com>
2023-10-17 11:39:24 +0100

    Dependency resolutions and minor changes

---
+++ Library/src/test/java/com/myproject/library/LibraryTests.java
@@ -0,0 +1,13 @@
+package com.myproject.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
